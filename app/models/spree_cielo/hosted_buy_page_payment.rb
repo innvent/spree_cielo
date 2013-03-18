b6b075627b731b4aa31b2b11ac855c08f370afd1
Rodrigo Gomes <rgomes.rodrigo@gmail.com>
2013-03-18 17:07:36 -0300

    Fix problem loading test environment.

--- app/models/spree_cielo/hosted_buy_page_payment.rb
+++ app/models/spree_cielo/hosted_buy_page_payment.rb
@@ -51,7 +51,7 @@ module SpreeCielo
 
       def api_number
         if preferences[:test_mode]
-          Cieloz::DadosEc::TEST_MOD_CIELO.numero
+          Cieloz::Homologacao::Credenciais::CIELO[:numero]
         else
           preferences[:api_number]
         end
@@ -59,7 +59,7 @@ module SpreeCielo
 
       def api_key
         if preferences[:test_mode]
-          Cieloz::DadosEc::TEST_MOD_CIELO.chave
+          Cieloz::Homologacao::Credenciais::CIELO[:chave]
         else
           preferences[:api_key]
         end
